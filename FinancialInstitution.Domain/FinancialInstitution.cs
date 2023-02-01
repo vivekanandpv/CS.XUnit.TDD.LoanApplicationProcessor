2c6dad04b27e18716078ec019aebf1d760c3e83e
Vivekanand P V <vivekanandpv@outlook.com>
2023-02-01 14:02:21 +0530

    Add project files.

---
+++ FinancialInstitution.Domain/FinancialInstitution.cs
@@ -0,0 +1,54 @@
+﻿namespace LoanApplicationProcessor.Domain
+{
+    public class FinancialInstitution
+    {
+        private readonly List<LoanApplication> _applications = new List<LoanApplication>();
+
+        public IEnumerable<LoanApplication> GetAllApplications()
+        {
+            return _applications;
+        }
+
+        public IEnumerable<LoanApplication> GetSubmittedApplications()
+        {
+            return _applications.Where(a => a.Status == ApplicationStatus.Submitted);
+        }
+
+        public void Submit(LoanApplication application)
+        {
+            _applications.Add(application);
+        }
+
+        public IEnumerable<LoanApplication> GetPendingVerificationApplications()
+        {
+            return _applications.Where(a => a.Status == ApplicationStatus.PendingVerification);
+        }
+
+        public IEnumerable<LoanApplication> GetVerificationFailedApplications()
+        {
+            return _applications.Where(a => a.Status == ApplicationStatus.VerificationFailed);
+        }
+
+        public IEnumerable<LoanApplication> GetApprovedApplications()
+        {
+            return _applications.Where(a => a.Status == ApplicationStatus.Approved);
+        }
+    }
+
+    public class LoanApplication
+    {
+        public ApplicationStatus Status { get; set; }
+    }
+
+    public enum ApplicationStatus
+    {
+        Submitted,
+        Resumbitted,
+        PendingVerification,
+        VerificationSuccess,
+        VerificationFailed,
+        PendingApproval,
+        Approved,
+        Rejected
+    }
+}
---
+++ FinancialInstitution.Tests/FinancialInstitutionShould.cs
@@ -0,0 +1,100 @@
+using LoanApplicationProcessor.Domain;
+
+namespace LoanApplicationProcessor.Tests
+{
+    public class FinancialInstitutionShould
+    {
+        [Fact]
+        public void ReturnSequenceOfAllApplications()
+        {
+            FinancialInstitution sut = new FinancialInstitution();
+            IEnumerable<LoanApplication> applications = sut.GetAllApplications();
+
+            Assert.IsAssignableFrom<IEnumerable<LoanApplication>>(applications);
+        }
+
+        [Fact]
+        public void ReturnSequenceOfAllSubmittedApplications()
+        {
+            FinancialInstitution sut = new FinancialInstitution();
+            
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.Approved});
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.Submitted});
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.PendingVerification});
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.Submitted});
+
+            IEnumerable<LoanApplication> submittedApplications = sut.GetSubmittedApplications();
+            Assert.All(
+                submittedApplications,
+                a =>
+                {
+                    Assert.True(a.Status == ApplicationStatus.Submitted);
+                });
+
+            Assert.Equal(2, submittedApplications.Count());
+        }
+
+        [Fact]
+        public void ReturnSequenceOfAllPendingVerificationApplications()
+        {
+            FinancialInstitution sut = new FinancialInstitution();
+            
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.Approved});
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.Submitted});
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.PendingVerification});
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.Submitted});
+
+            IEnumerable<LoanApplication> applications = sut.GetPendingVerificationApplications();
+            Assert.All(
+                applications,
+                a =>
+                {
+                    Assert.True(a.Status == ApplicationStatus.PendingVerification);
+                });
+
+            Assert.Equal(1, applications.Count());
+        }
+
+        [Fact]
+        public void ReturnSequenceOfAllVerificationFailedApplications()
+        {
+            FinancialInstitution sut = new FinancialInstitution();
+            
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.Approved});
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.Submitted});
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.PendingVerification});
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.VerificationFailed});
+
+            IEnumerable<LoanApplication> applications = sut.GetVerificationFailedApplications();
+            Assert.All(
+                applications,
+                a =>
+                {
+                    Assert.True(a.Status == ApplicationStatus.VerificationFailed);
+                });
+
+            Assert.Equal(1, applications.Count());
+        }
+
+        [Fact]
+        public void ReturnSequenceOfAllApprovedApplications()
+        {
+            FinancialInstitution sut = new FinancialInstitution();
+            
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.Approved});
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.Submitted});
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.PendingVerification});
+            sut.Submit(new LoanApplication{Status = ApplicationStatus.VerificationFailed});
+
+            IEnumerable<LoanApplication> applications = sut.GetApprovedApplications();
+            Assert.All(
+                applications,
+                a =>
+                {
+                    Assert.True(a.Status == ApplicationStatus.Approved);
+                });
+
+            Assert.Equal(1, applications.Count());
+        }
+    }
+}
